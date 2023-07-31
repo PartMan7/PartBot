d366780afe182e28e8b87f060dda67b75bdc68c7
PartMan <mane.parth@gmail.com>
2023-07-31 15:29:28 +0530

    Merge deployments into a single shell sscript

--- deploy-public.sh
+++
@@ -1,7 +0,0 @@
-# Deploy
-git add .
-git commit -m "${*}"
-git push origin main
-
-
-# Windows people SHOOO this file isn't for you
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,24 @@
-./deploy-public.sh $@
-./deploy-prod.sh $@
+# Deploy to public repository
+git add .
+git commit -m "${*}"
+git push origin main
+
+
+# Deploy to deployment repository
+
+# Get number of lines to modify in .gitignore
+gitlines=$(grep -nm 1 ^$ .gitignore | cut -c1 | tr -d $'\n')
+gitlines=$((gitlines-1))
+# Modify .gitignore
+sed -i "1,$gitlines s/^/# /1" .gitignore
+
+# Push
+git --git-dir=.git-deploy add .
+git --git-dir=.git-deploy commit -m "${*}"
+git --git-dir=.git-deploy push origin main
+
+# Restore .gitignore
+sed -i "1,$gitlines s/# //1" .gitignore
+
+
+# Windows people SHOOO this file isn't for you
