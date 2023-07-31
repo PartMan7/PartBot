7781ac9685879b2f56ac9befa0b85516d85261f1
PartMan <mane.parth@gmail.com>
2023-07-31 11:55:55 +0530

    Publish @ 2023-07-31_11:55:55

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,5 @@ timestamp=$(date '+%F_%T')
 git commit -m "Publish @ $timestamp"
 git push origin main
 
-# Restore .gitignore
-sed -i "1,$gitlines s/# //1" .gitignore
-
 
 # Windows people SHOOO this file isn't for you
