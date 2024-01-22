62c921f6fa3182aa506b651752b489b49afe7e79
Parth Mane <parth.mane@sprinklr.com>
2024-01-22 11:44:09 +0530

    chore: Add context to eval

--- src/ps/commands/eval.ts
+++ src/ps/commands/eval.ts
@@ -6,8 +6,9 @@ export const command: PSCommand = {
 	syntax: 'CMD code',
 	perms: 'admin',
 	aliases: ['exec', 'run'],
-	async run (message, { arg, originalCommand: [originalCommand] }) {
-		const res = await evaluate(arg, originalCommand === 'exec' ? 'ABBR_OUTPUT' : 'COLOR_OUTPUT', { message });
+	async run (message, context) {
+		const { arg, originalCommand: [originalCommand] } = context;
+		const res = await evaluate(arg, originalCommand === 'exec' ? 'ABBR_OUTPUT' : 'COLOR_OUTPUT', { message, context });
 		if (originalCommand === 'eval') message.replyHTML(`<br/>${res.output}`); // Add a slight gap
 		else if (originalCommand === 'run') message.sendHTML(res.output);
 		else return message.reply(res.success ? `Command executed successfully` : `Error in executing command: ${res.output}`);
