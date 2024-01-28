a2f40bf26e762cfc9139e08ea1e3d6229ff376ec
PartMan <mane.parth@gmail.com>
2024-01-28 16:27:07 +0530

    chore: Add 'cache' to eval context

--- src/ps/commands/eval.ts
+++ src/ps/commands/eval.ts
@@ -1,4 +1,5 @@
 import { evaluate } from 'utils/eval';
+import * as cache from 'cache';
 
 export const command: PSCommand = {
 	name: 'eval',
@@ -8,7 +9,7 @@ export const command: PSCommand = {
 	aliases: ['exec', 'run'],
 	async run (message, context) {
 		const { arg, originalCommand: [originalCommand] } = context;
-		const res = await evaluate(arg, originalCommand === 'exec' ? 'ABBR_OUTPUT' : 'COLOR_OUTPUT', { message, context });
+		const res = await evaluate(arg, originalCommand === 'exec' ? 'ABBR_OUTPUT' : 'COLOR_OUTPUT', { message, context, cache });
 		if (originalCommand === 'eval') message.replyHTML(`<br/>${res.output}`); // Add a slight gap
 		else if (originalCommand === 'run') message.sendHTML(res.output);
 		else return message.reply(res.success ? `Command executed successfully.` : `Error in executing command: ${res.output}`);
