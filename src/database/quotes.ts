fa43b625746f2d74e3d0313d9a3d6792d18f04f1
PartMan <mane.parth@gmail.com>
2024-01-28 16:27:21 +0530

    feat: Start working on quotes

---
+++ src/database/quotes.ts
@@ -0,0 +1,64 @@
+import mongoose from 'mongoose';
+
+const schema = new mongoose.Schema({
+	quote: {
+		type: String,
+		required: true
+	},
+	rawText: {
+		type: String,
+		required: true,
+		default: function () {
+			return (this.quote as string).toLowerCase().replace(/[^a-zA-Z0-9]+/g, ' ').trim();
+		}
+	},
+	room: {
+		type: String,
+		required: true
+	},
+	addedBy: {
+		type: String,
+		required: true
+	},
+	addedById: {
+		type: String,
+		required: true,
+		default: function () {
+			return Tools.toId(this.addedBy);
+		}
+	},
+	at: {
+		type: Date,
+		required: true,
+		default: Date.now
+	}
+});
+
+interface Model {
+	quote: string;
+	rawText: string;
+	room: string;
+	addedBy: string;
+	addedById: string;
+	at: Date;
+}
+const model = mongoose.model('quote', schema, 'quotes');
+
+export function addQuote (quote: string, room: string, by: string): Promise<Model> {
+	return model.create({ quote, room, addedBy: by });
+}
+
+export function getAllQuotes (room: string): Promise<Model[]> {
+	return model.find({ room }).sort({ at: 1 }).lean();
+}
+
+export async function getQuoteByIndex (index: number, room: string): Promise<Model | null> {
+	const quotes = await getAllQuotes(room);
+	return quotes[index] ?? null;
+}
+
+export function searchQuotes (room: string, filter: Partial<Omit<Model, 'room' | 'quote' | 'rawText'>> & {
+	quote?: RegExp;
+}): Promise<Model[]> {
+	return model.find({ room, ...filter, quote: undefined, rawText: filter.quote }).sort({ at: 1 }).lean();
+}
--- src/ps/commands/quotes.tsx
+++ src/ps/commands/quotes.tsx
@@ -1,20 +1,74 @@
-function Test ({ children }) {
-	return <div>
-		<h3>Header</h3>
-		<span>
-			{'<b>This should not be bold</b>'}
-			<br/>
-			{children}
-		</span>
-	</div>;
-}
+import { prefix } from 'config/ps';
 
 export const command: PSCommand = {
 	name: 'quotes',
 	aliases: ['q', 'quote'],
-	help: 'Quotes!',
+	help: null,
 	syntax: 'CMD',
-	async run (message) {
-		message.sendHTML(<Test>This is in JSX</Test>);
+	children: {
+		help: {
+			name: 'help',
+			aliases: ['h'],
+			help: 'Remove the extra \'help\'...',
+			syntax: 'CMD',
+			async run (message, { broadcastHTML }) {
+				broadcastHTML(<ul>
+					<li>
+						<b>(term / index)</b>:
+						Displays a random quote with the specified search term / at the given index.
+					</li>
+					<li>
+						<b>help [h]</b>:
+						Displays this message.
+					</li>
+					<li>
+						<b>random [r]</b>:
+						Displays a random quote.
+					</li>
+					<li>
+						<b>find [f]</b>:
+						Displays all quotes with the specified search term.
+					</li>
+					<li>
+						<b>last [z]</b>:
+						Displays the latest quote added.
+					</li>
+					<li>
+						<b>list [l]</b>:
+						Displays a list (up to 50 quotes per list). If given a number, specifically looks up that list.
+					</li>
+					<li>
+						<b>page [g]</b>:
+						Sends the user an HTML page (up to 50 quotes per page).
+					</li>
+					<li>
+						<b>number [n]</b>:
+						Displays the number of quotes in the room.
+					</li>
+					<li>
+						<b>preview [p]</b>:
+						Displays a preview of the given quote (without actually adding it)
+					</li>
+					<li>
+						<b>add [a]</b>:
+						Adds the given quote. <code>\n</code> works as a newline, and <code>/me</code> syntax can be
+						formatted via wrapping the username in <code>[]</code> (eg: <code>[14:20:21] • #PartMan hugs
+						Hydro</code> would be formatted as <code>[14:20:21] • #[PartMan] hugs Hydro</code>) (staff-only)
+					</li>
+					<li>
+						<b>delete [d/x]</b>:
+						Deletes the given quote. Accepts either an index or a lookup term (<code>z</code> deletes the last)
+					</li>
+					<li>
+						<b>room [m]</b>:
+						Runs the command in the context of the given room. Eg: <code>{prefix}quote room Bot Development
+						| list</code>
+					</li>
+				</ul>);
+			}
+		}
+	},
+	async run (message, { run }) {
+		return run('quote random');
 	}
 };
