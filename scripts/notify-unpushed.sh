c37e6ac97499b9d7c108c83531ecc5185e489e92
Parth Mane <parth.mane@sprinklr.com>
2024-10-05 13:00:58 +0530

    chore: Fix logic

--- scripts/notify-unpushed.sh
+++ scripts/notify-unpushed.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
 echo "Checking for pending changes..."
-if [ -z "$1" ]; then cd "$1" || :; fi
+if [ -n "$1" ]; then cd "$1" || :; fi
 
 if git status --porcelain | grep '.*'
 then
-  printf "\nPending changes found."
+  echo "\nPending changes found."
   if ! [ -z "$FAIL_ON_PENDING" ]; then exit 1; fi
 else
   echo "No pending changes found."
