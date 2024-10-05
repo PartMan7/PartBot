52243467a0176338cbcfa7a89acfe565053a1552
Parth Mane <parth.mane@sprinklr.com>
2024-10-05 12:53:26 +0530

    chore: Fix FAIL_ON_PENDING logic

--- scripts/notify-unpushed.sh
+++ scripts/notify-unpushed.sh
@@ -6,7 +6,7 @@ if [ -z "$1" ]; then cd "$1" || :; fi
 if git status --porcelain | grep '.*'
 then
   printf "\nPending changes found."
-  if [ -z "$FAIL_ON_PENDING" ]; then exit 1; fi
+  if ! [ -z "$FAIL_ON_PENDING" ]; then exit 1; fi
 else
   echo "No pending changes found."
 fi
