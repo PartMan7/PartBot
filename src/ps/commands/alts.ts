74a9d0570f7d2070af806f333c3b908409790373
Parth Mane <parth.mane@sprinklr.com>
2024-10-29 02:24:45 +0530

    commands: Add support for checking other people's alts

--- src/ps/commands/alts.ts
+++ src/ps/commands/alts.ts
@@ -1,12 +1,19 @@
 import { getAlts } from '@/database/alts';
+import { checkPermissions } from '@/ps/handlers/permissions';
+import { ACCESS_DENIED } from '@/text';
 
 export const command: PSCommand = {
 	name: 'alts',
 	help: 'Testing command',
 	syntax: 'CMD USERNAME?',
 	aliases: ['getalts'],
-	async run({ message }) {
-		const altsList = await getAlts(message.author.userid);
+	async run({ message, arg }) {
+		let lookup = message.author.userid;
+		if (arg) {
+			if (!checkPermissions(['room', 'driver'], message)) throw new ChatError(ACCESS_DENIED);
+			lookup = Tools.toId(arg);
+		}
+		const altsList = await getAlts(lookup);
 		message.reply(`Alts: ${altsList.join(', ')}`);
 	},
 };
