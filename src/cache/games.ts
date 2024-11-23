2ecce3efd3d32effd878ff6070b0ae3023da3d57
Parth Mane <parth.mane@sprinklr.com>
2024-11-23 18:28:13 +0530

    chore: ...why was that a function

--- src/cache/games.ts
+++ src/cache/games.ts
@@ -7,38 +7,32 @@ export type GameCache = {
 	set(id: string, arg: string): void;
 };
 
-let gameCache: GameCache | null = null;
+const cacheId = 'games.json';
+const flatCache = new FlatCache({ cacheId: cacheId, cacheDir: fsPath('cache', 'flat-cache') });
+flatCache.load(cacheId);
 
-export function useGameCache(): GameCache {
-	if (gameCache) return gameCache;
-	const cacheId = 'games.json';
-	const flatCache = new FlatCache({ cacheId: cacheId, cacheDir: fsPath('cache', 'flat-cache') });
-	flatCache.load(cacheId);
-
-	gameCache = {
-		get(id) {
-			const lookup = flatCache.get<string>(id);
-			if (!lookup) throw new Error(`Attempting to get ${id} but nothing found.`);
-			return lookup;
-		},
-		getByGame(game) {
-			const flatCacheObj = flatCache.all();
-			const unfilteredGames = Object.values(flatCacheObj);
-			if (game === 'all') return unfilteredGames;
-			return unfilteredGames.filter(gameLog => {
-				try {
-					const parsed = JSON.parse(gameLog);
-					return 'meta' in parsed && 'id' in parsed.meta && parsed.meta.id === game;
-				} catch (e) {
-					log(`Error parsing backups for: ${gameLog}`, e);
-					return false;
-				}
-			});
-		},
-		set(id, value) {
-			flatCache.set(id, value);
-			flatCache.save();
-		},
-	};
-	return gameCache;
-}
+export const gameCache: GameCache = {
+	get(id) {
+		const lookup = flatCache.get<string>(id);
+		if (!lookup) throw new Error(`Attempting to get ${id} but nothing found.`);
+		return lookup;
+	},
+	getByGame(game) {
+		const flatCacheObj = flatCache.all();
+		const unfilteredGames = Object.values(flatCacheObj);
+		if (game === 'all') return unfilteredGames;
+		return unfilteredGames.filter(gameLog => {
+			try {
+				const parsed = JSON.parse(gameLog);
+				return 'meta' in parsed && 'id' in parsed.meta && parsed.meta.id === game;
+			} catch (e) {
+				log(`Error parsing backups for: ${gameLog}`, e);
+				return false;
+			}
+		});
+	},
+	set(id, value) {
+		flatCache.set(id, value);
+		flatCache.save();
+	},
+};
