311da27c910d54fe39991d71c6cf407cdf9c9818
PartMan <mane.parth@gmail.com>
2025-02-07 00:35:08 +0530

    chore: Don't crash if message.author is undefined

--- src/ps/handlers/page.ts
+++ src/ps/handlers/page.ts
@@ -2,7 +2,7 @@ import type { PSMessage } from '@/types/ps';
 
 export default function pageHandler(message: PSMessage) {
 	// Do stuff
-	if (message.isIntro || !message.author.userid || !message.target) return;
+	if (message.isIntro || !message.author?.userid || !message.target) return;
 	if (message.type !== 'pm') return;
 	if (!message.content.startsWith('|requestpage|')) return;
 }
