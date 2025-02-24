3120c178bc81fb716cba45c06f4c8652b7f421ce
Parth Mane <parth.mane@sprinklr.com>
2025-02-25 00:57:26 +0530

    chore: Update port for NGINX config getter

--- scripts/build_nginx_confs.sh
+++ scripts/build_nginx_confs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 for file in src/web/configs/*; do
-  sed "s#{{PWD}}#$PWD#g;s#{{PORT}}#${PORT:-80}#g" "$file" > "${file//.base/}"
+  sed "s#{{PWD}}#$PWD#g;s#{{PORT}}#${PORT:-8800}#g" "$file" > "${file//.base/}"
 done
