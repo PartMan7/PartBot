abf1a2b3a0cbc178cd14b55d10a3b5f79229a583
Parth Mane <parth.mane@sprinklr.com>
2025-05-22 03:48:54 +0530

    chore: Cleanup

--- src/ps/loaders/roomconfigs.ts
+++ src/ps/loaders/roomconfigs.ts
@@ -1,11 +1,11 @@
-import { PSRoomConfigs as RoomConfigCache } from '@/cache';
+import { PSRoomConfigs } from '@/cache';
 import { fetchRoomConfigs } from '@/database/psrooms';
 import { log } from '@/utils/logger';
 
 export async function loadRoomConfigs(): Promise<void> {
 	const fetched = await fetchRoomConfigs();
 	fetched.forEach(entry => {
-		RoomConfigCache[entry.roomId] = entry;
+		PSRoomConfigs[entry.roomId] = entry;
 	});
 	log('Loaded PS room configs!');
 }
