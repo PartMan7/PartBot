09a98a075ebe9425737cf84a396a017d7331bb07
Parth Mane <parth.mane@sprinklr.com>
2025-05-26 15:47:25 +0530

    chore: Sample default max should be 9

--- src/ps/commands/sample.ts
+++ src/ps/commands/sample.ts
@@ -6,10 +6,10 @@ import type { PSCommand } from '@/types/chat';
 export const command: PSCommand = {
 	name: 'sample',
 	help: 'Generates random numbers.',
-	syntax: 'CMD [max=10] x [count=5]',
+	syntax: 'CMD [max=9] x [count=5]',
 	aliases: ['random'],
 	async run({ arg, broadcast, originalCommand }) {
-		const [max, count] = parsePoint(arg) ?? [10, 5];
+		const [max, count] = parsePoint(arg) ?? [9, 5];
 		const nums =
 			originalCommand[0] === 'random'
 				? Array.from({ length: count }, () => sample(max) + 1)
