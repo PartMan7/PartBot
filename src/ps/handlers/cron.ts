d2fa5bd0c7d1f31f2cc5b0d4902099033b5985cc
Parth Mane <parth.mane@sprinklr.com>
2025-05-26 23:59:58 +0530

    cron: Convert Hindi CRON job to production

--- src/ps/handlers/cron.ts
+++ src/ps/handlers/cron.ts
@@ -27,14 +27,13 @@ export class PSCronJobManager {
 export function startPSCron(this: Client): PSCronJobManager {
 	const Jobs = new PSCronJobManager();
 
-	// TODO Move back to Hindi and remove 'CRON:'
 	Jobs.register('hindi-automodchat-enable', '0 0 * * *', TimeZone.IST, () => {
-		this.rooms.get('botdevelopment')?.send('CRON: /automodchat 10, +');
+		this.rooms.get('hindi')?.send('/automodchat 10, +');
 	});
 	Jobs.register('hindi-automodchat-disable', '0 7 * * *', TimeZone.IST, () => {
-		const room = this.rooms.get('botdevelopment');
-		room?.send('CRON: /modchat ac');
-		room?.send('CRON: /automodchat off');
+		const room = this.rooms.get('hindi');
+		room?.send('/modchat ac');
+		room?.send('/automodchat off');
 	});
 
 	// Kill existing cron jobs
