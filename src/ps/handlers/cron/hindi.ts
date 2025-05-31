03052ea58de0e2465633c4f94c13930734b1bd6f
Parth Mane <parth.mane@sprinklr.com>
2025-06-01 04:29:16 +0700

    feat: Add in Hindi/Galli Galli Sim Sim schedules

--- src/ps/handlers/cron/hindi.ts
+++ src/ps/handlers/cron/hindi.ts
@@ -1,7 +1,10 @@
+import { pokedex } from 'ps-client/data';
+
 import { PSCommands } from '@/cache';
 import { bulkAddPoints } from '@/database/points';
 import { i18n } from '@/i18n';
 import { TimeZone } from '@/ps/handlers/cron/constants';
+import { sample } from '@/utils/random';
 import { sleep } from '@/utils/sleep';
 
 import type { PSCronJobManager } from '@/ps/handlers/cron/index';
@@ -9,6 +12,65 @@ import type { PSCommandContext } from '@/types/chat';
 import type { RecursivePartial } from '@/types/common';
 import type { PSMessage } from '@/types/ps';
 import type { Client, User } from 'ps-client';
+
+function _preGGSSEvent(this: Client, eventType: string) {
+	const hindiRoom = this.getRoom('hindi');
+	const ggssRoom = this.getRoom('galligallisimsim');
+	if (!hindiRoom || !ggssRoom) return;
+
+	hindiRoom.send(`/wall ${eventType} in <<galligallisimsim>>!`);
+	ggssRoom.send('/modchat ac');
+	ggssRoom.send('/declare HRMC!');
+}
+
+function _postGGSSEvent(this: Client) {
+	const ggssRoom = this.getRoom('galligallisimsim');
+	if (!ggssRoom) return;
+
+	ggssRoom.send('Khelne ke liye shukriya!'); // TODO: Use i18n instead
+	ggssRoom.send('/modchat +');
+	if (Math.random() < 0.1) {
+		const randomAuth = ggssRoom.users.filter(user => ![' ', '*'].includes(user.charAt(0))).random();
+		if (!randomAuth) return;
+		ggssRoom.send(`${randomAuth.slice(1)} hi nerd`);
+	}
+}
+
+async function AnimeQuiz(this: Client) {
+	const room = this.getRoom('galligallisimsim');
+	if (!room) return;
+
+	_preGGSSEvent.call(this, 'Anime Quiz');
+	room.send('Anime Quiz ek minute mei shuru hoga!');
+
+	await sleep('1 min');
+	room.send(']trivia start 15 -cx ecchi -s 20 -d 5');
+}
+
+async function Hangman(this: Client) {
+	const room = this.getRoom('galligallisimsim');
+	if (!room) return;
+
+	_preGGSSEvent.call(this, 'Hangman');
+
+	const HANGMAN_GAMES_COUNT = 20;
+	room.send('Hangman ek minute mei shuru hoga!');
+	await sleep('1 min');
+
+	let gamesDone = 0;
+	while (gamesDone < HANGMAN_GAMES_COUNT) {
+		// TODO: Add a hangman command
+		const randMon = Object.values(pokedex)
+			.filter(mon => mon.num > 0 && !mon.forme)
+			.random();
+		room.send(`/hangman create ${randMon.name}, A Pokémon!`);
+
+		gamesDone++;
+		await sleep('20s');
+	}
+
+	_postGGSSEvent.call(this);
+}
 
 export function register(this: Client, Jobs: PSCronJobManager): void {
 	// Main room
@@ -21,31 +83,46 @@ export function register(this: Client, Jobs: PSCronJobManager): void {
 		room?.send('/automodchat off');
 	});
 
-	const preGGSSEvent = (eventType: string) => {
-		const hindiRoom = this.getRoom('hindi');
-		const ggssRoom = this.getRoom('galligallisimsim');
-		if (!hindiRoom || !ggssRoom) return;
-
-		hindiRoom.send(`/wall ${eventType} in <<galligallisimsim>>!`);
-		ggssRoom.send('/modchat ac');
-		ggssRoom.send('/declare HRMC!');
-	};
-
-	const postGGSSEvent = () => {
-		const ggssRoom = this.getRoom('galligallisimsim');
-		if (!ggssRoom) return;
-
-		ggssRoom.send('Khelne ke liye shukriya!'); // TODO: Use i18n instead
-		ggssRoom.send('/modchat +');
-		if (Math.random() < 0.1) {
-			const randomAuth = ggssRoom.users.filter(user => ![' ', '*'].includes(user.charAt(0))).random();
-			if (!randomAuth) return;
-			ggssRoom.send(`${randomAuth.slice(1)} hi nerd`);
-		}
-	};
+	// TODO: RkR, DkR
 
 	// Galli Galli Sim Sim
-	Jobs.register('ggss-kunc', '30 21 * * Wed,Sat', TimeZone.IST, () => {
+	const preGGSSEvent = _preGGSSEvent.bind(this);
+	const postGGSSEvent = _postGGSSEvent.bind(this);
+
+	Jobs.register('ggss-day-animequiz', '0 17 * * Wed', TimeZone.IST, AnimeQuiz.bind(this));
+
+	Jobs.register('ggss-day-hangman', '0 17 * * Sat', TimeZone.IST, Hangman.bind(this));
+
+	Jobs.register('ggss-randblitz', '0 19 * * *', TimeZone.IST, () => {
+		const room = this.getRoom('galligallisimsim');
+		if (!room) return;
+
+		const currentGen = 9;
+		const gen = sample(currentGen) + 1;
+		const base = `[Gen ${gen}] Random Battle`;
+
+		preGGSSEvent('Blitz tournament');
+		room.send(`/tour create ${base}, elim\n/tour rules Blitz\n/tour name ${base} Blitz`);
+		room.send(`/tour forcetimer on`);
+		room.send(`/tour autostart 7`);
+		room.send(`/tour autodq 2`);
+		// TODO: End when tour end is received
+	});
+
+	Jobs.register('ggss-night-uno', '30 21 * * Sun,Tue', TimeZone.IST, async () => {
+		const room = this.getRoom('galligallisimsim');
+		if (!room) return;
+		preGGSSEvent('Uno');
+		room.send('/uno create');
+		room.send('/uno autostart 60');
+
+		await sleep('10 min'); // TODO: Hook to Uno raw events
+		postGGSSEvent();
+	});
+
+	Jobs.register('ggss-night-hangman', '30 21 * * Mon,Thu', TimeZone.IST, Hangman.bind(this));
+
+	Jobs.register('ggss-night-kunc', '30 21 * * Wed,Sat', TimeZone.IST, async () => {
 		const room = this.getRoom('galligallisimsim');
 		if (!room) return;
 		const kuncCommand = PSCommands.kunc;
@@ -60,36 +137,36 @@ export function register(this: Client, Jobs: PSCronJobManager): void {
 		};
 		const partialContext: Partial<PSCommandContext> = { arg: '', message: partialMessage as PSMessage, $T };
 
-		async function event(rounds: number) {
-			preGGSSEvent('Kunc');
-			room.send(`Kunc starting in 1 minute!`); // TODO i18n this
-			await sleep('1 min');
-			room.send('Starting kunc!');
+		const rounds = 10;
 
-			for (let i = 0; i < rounds; i++) {
-				const winners = (await kuncCommand.run(partialContext as PSCommandContext)) as User[] | null;
-				if (winners) {
-					const pointsData = Object.fromEntries(
-						winners.map(user => [
-							user.id,
-							{
-								name: user.name,
-								id: user.id,
-								points: {
-									points: 1,
-									officialPoints: 1,
-								},
+		preGGSSEvent('Kunc');
+		room.send(`Kunc starting in 1 minute!`); // TODO i18n this
+		await sleep('1 min');
+		room.send('Starting kunc!');
+
+		for (let i = 0; i < rounds; i++) {
+			const winners = (await kuncCommand.run(partialContext as PSCommandContext)) as User[] | null;
+			if (winners) {
+				const pointsData = Object.fromEntries(
+					winners.map(user => [
+						user.id,
+						{
+							name: user.name,
+							id: user.id,
+							points: {
+								points: 1,
+								officialPoints: 1,
 							},
-						])
-					);
-					bulkAddPoints(pointsData, room.id);
-				}
-				if (i !== rounds - 1) await sleep('5 sec');
+						},
+					])
+				);
+				bulkAddPoints(pointsData, room.id);
 			}
-
-			postGGSSEvent();
+			if (i !== rounds - 1) await sleep('5 sec');
 		}
 
-		event(10);
+		postGGSSEvent();
 	});
+
+	Jobs.register('ggss-night-animequiz', '30 21 * * Fri', TimeZone.IST, AnimeQuiz.bind(this));
 }
