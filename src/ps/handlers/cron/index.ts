7bfa812855dbd7c4dee520e5536edda8be36396e
Parth Mane <parth.mane@sprinklr.com>
2025-06-04 23:05:06 +0700

    chore: Make CRON jobs accessible

--- src/ps/handlers/cron/index.ts
+++ src/ps/handlers/cron/index.ts
@@ -7,14 +7,14 @@ import type { TimeZone } from '@/ps/handlers/cron/constants';
 import type { Client } from 'ps-client';
 
 export class PSCronJobManager {
-	readonly #jobs: Record<string, CronJob> = {};
+	jobs: Record<string, CronJob> = {};
 
 	register(id: string, cronTime: string, timeZone: TimeZone, callback: () => void): void {
-		this.#jobs[id] = CronJob.from({ name: id, cronTime, start: true, onTick: callback, timeZone });
+		this.jobs[id] = CronJob.from({ name: id, cronTime, start: true, onTick: callback, timeZone });
 	}
 	kill(): void {
-		for (const jobId in this.#jobs) {
-			this.#jobs[jobId].stop();
+		for (const jobId in this.jobs) {
+			this.jobs[jobId].stop();
 		}
 	}
 }
