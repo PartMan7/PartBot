d0d1fa607fc39c71f6f8e733ae68128516a56d11
Parth Mane <parth.mane@sprinklr.com>
2025-06-05 11:10:21 +0700

    sentinel: Add a rejection handler

--- src/sentinel/process.ts
+++ src/sentinel/process.ts
@@ -8,11 +8,16 @@ export function processHandler(): { kill: () => void } {
 		// Unfortunately, I like living life on the edge
 		// We stay alive!
 	}
+	function rejectionHandler(reason: unknown, _rejectedPromise: Promise<unknown>) {
+		errorLog(new Error(`A promise failed with reason: ${reason}`));
+	}
 	process.on('uncaughtException', errorHandler);
+	process.on('unhandledRejection', rejectionHandler);
 
 	return {
 		kill: () => {
 			process.removeListener('uncaughtException', errorHandler);
+			process.removeListener('unhandledRejection', rejectionHandler);
 		},
 	};
 }
