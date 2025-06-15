f3cb6cb5a52be809e7d64eadb1c795dd2d2f1c6b
Parth Mane <parth.mane@sprinklr.com>
2025-06-15 23:32:25 +0530

    chore: Add autores for PartBot?

--- src/ps/handlers/autores.ts
+++ src/ps/handlers/autores.ts
@@ -1,5 +1,12 @@
+import { prefix } from '@/config/ps';
+import { toId } from '@/tools';
+
 import type { PSMessage } from '@/types/ps';
 
 export function autoResHandler(message: PSMessage) {
 	if (!message.author.userid || !message.target) return;
+	if (toId(message.content) === message.parent.status.userid && message.content.endsWith('?'))
+		message.author.send(
+			`Hi, I'm ${message.parent.status.username}! I'm a bot by PartMan. Please try \`\`${prefix}help\`\` for more info!`
+		);
 }
