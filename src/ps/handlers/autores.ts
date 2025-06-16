73ffcd3dd11cf00a26900088dc6e71eb582ac3b4
Parth Mane <parth.mane@sprinklr.com>
2025-06-16 10:45:17 +0530

    autores: Add isIntro check and dhelmise stuff

--- src/ps/handlers/autores.ts
+++ src/ps/handlers/autores.ts
@@ -3,10 +3,15 @@ import { toId } from '@/tools';
 
 import type { PSMessage } from '@/types/ps';
 
+const HELP_MESSAGE = `I'm a bot by PartMan. Please try \`\`${prefix}help\`\` for more info!`;
+
 export function autoResHandler(message: PSMessage) {
+	if (message.isIntro) return;
 	if (!message.author.userid || !message.target) return;
-	if (toId(message.content) === message.parent.status.userid && message.content.endsWith('?'))
-		message.author.send(
-			`Hi, I'm ${message.parent.status.username}! I'm a bot by PartMan. Please try \`\`${prefix}help\`\` for more info!`
-		);
+	if (toId(message.content) === message.parent.status.userid && message.content.endsWith('?')) {
+		return message.author.send(`Hi, I'm ${message.parent.status.username}! ${HELP_MESSAGE}`);
+	}
+	if (message.author.id === 'dhelmise' && message.content.trim().toLowerCase() === 'fartbot?') {
+		return message.reply(`I'm FartBot! ${HELP_MESSAGE}.\nAlso, you need a shower.`);
+	}
 }
