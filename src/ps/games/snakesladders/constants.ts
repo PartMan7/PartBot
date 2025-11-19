1e68c9c9d6ff2bbec161dfb36e10bafd2f12850f
TanPat <tanishpatil30xe@gmail.com>
2025-11-19 17:47:20 +0530

    chore:

--- src/ps/games/snakesladders/constants.ts
+++ src/ps/games/snakesladders/constants.ts
@@ -1,5 +1,5 @@
-import {StringToHex} from '@/utils/color';
+import { StringToHex } from '@/utils/color';
 
 export const TOKEN_COLORS = ['#ff0000', '#ff8000', '#ffff00', '#00ff00', '#00ffff', '#0000ff', '#9e00ff', '#ff00ff'].map(
-    color => StringToHex(color)!
+	color => StringToHex(color)!
 );
