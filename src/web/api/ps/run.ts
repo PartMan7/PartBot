838baebbacc9ad6546773bdb1dcdb0b3f5d1bf7a
PartMan <parth.mane@sprinklr.com>
2026-05-29 21:19:39 +0530

    chore: Support running commands on PS via web

---
+++ src/web/api/ps/run.ts
@@ -0,0 +1,17 @@
+import PS from '@/ps';
+import { WebError } from '@/utils/webError';
+
+import type { RequestHandler } from 'express';
+
+const ACCESS_TOKEN = process.env.WEB_PS_ACCESS_TOKEN;
+
+export const handler: RequestHandler = async (req, res) => {
+	const { room, text, accessToken } = req.body as { room?: string; text?: string; accessToken?: string };
+	if (!room || !text || !accessToken) throw new WebError(400);
+	if (!ACCESS_TOKEN) throw new WebError(500);
+	if (accessToken !== ACCESS_TOKEN) throw new WebError(401);
+	const roomObj = PS.getRoom(room);
+	if (!roomObj) throw new WebError(404);
+	roomObj.send(text);
+	res.json({ success: true });
+};
