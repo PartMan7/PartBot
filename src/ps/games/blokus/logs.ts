a1edc1b5b70325a332e4e30fa57a23e0e57a4984
PartMan <parth.mane@sprinklr.com>
2026-08-27 10:38:59 +0530

    chore: Fix logs

--- src/ps/games/blokus/logs.ts
+++ src/ps/games/blokus/logs.ts
@@ -2,4 +2,14 @@ import type { PieceId } from '@/ps/games/blokus/constants';
 import type { Turn } from '@/ps/games/blokus/types';
 import type { BaseLog } from '@/ps/games/types';
 
-export type Log = { action: 'play'; time: Date; turn: Turn; ctx: { piece: PieceId; anchor: [number, number] } } & BaseLog;
+export type Log = {
+	action: 'play';
+	time: Date;
+	turn: Turn;
+	ctx: {
+		piece: PieceId;
+		orientation: number;
+		anchor: [number, number];
+		cells: [number, number][];
+	};
+} & BaseLog;
